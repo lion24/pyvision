ffe4337d8694aa0af7ee42ff6be57833ad62f243
Lionel Hubaut <lionel.hubaut@dekimo.com>
2024-06-28 21:40:04 +0200

    refactor(fps): futher optimize fps calculation
    FPS can also be used to throlling. Hence we can now throttling directly
    the backend from the front by specifying how much FPS we want to get.
    
    Signed-off-by: Lionel Hubaut <lionel.hubaut@dekimo.com>

--- src/pyvision/camera/fps.py
+++ src/pyvision/camera/fps.py
@@ -1,23 +1,30 @@
-from datetime import datetime
+import timeit
+import time
 
 class FPS:
-    def __init__(self):
-        self._start : datetime = None
-        self._end : datetime = None
+    def __init__(self, throttle_fps: int = 30):
+        self.prev_time = timeit.default_timer()
+        self.total_time = 0.0
         self.num_frames = 0
-
-    def start(self):
-        self._start = datetime.now()
-        return self
+        self.throttle_fps = throttle_fps
+        self.fps = 0
     
-    def stop(self):
-        self._end = datetime.now()
+    def update_time(self):
+        current_time = timeit.default_timer()
+        self.delta_time = current_time - self.prev_time
+        self.total_time += self.delta_time
+        self.prev_time = current_time
     
-    def update(self):
+    def update(self, throttle: bool = False):
+        self.update_time()
         self.num_frames += 1
+        self.fps = self.num_frames / self.total_time
 
-    def elapsed(self):
-        return (self._end - self._start).total_seconds()
-    
-    def fps(self):
-        return self.num_frames / self.elapsed()
+        if throttle:
+            sleep_time = (1.0 / self.throttle_fps) - self.delta_time
+            if sleep_time > 0.0:
+                time.sleep(sleep_time)
+                self.update_time()
+
+    def get_fps(self):
+        return self.fps
